bd32ebf8c5c87bd793f78495a8fac9bcfa94f24a
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-09-19 23:08:50 +0300

    variablesss

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Jack Bauer";
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + "!" ;
+const shortGreeting = `Welcome, ${currentUser[0]}!`
